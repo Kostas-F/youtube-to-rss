a9500ede4aa71ccf646f939501a7f6075e452445
Kostas-F <43196039+Kostas-F@users.noreply.github.com>
2022-06-02 19:07:47 +0300

    Added video link crawling functionality

--- channelsToCSV.py
+++ channelsToCSV.py
@@ -3,11 +3,14 @@ import youtubeToRSS as ytRSS
 
 # Use a given link to lookup channel names by looking for a specific spot in the 
 # youtube page.
-def crawlForUserName(link):
+def crawlForUserName(link,video=False):
 	# I'm not using BeautifulSoup because i cant seem to isolate the channel name within
 	# tags or anything of the sort. 
 	# If anyone can get this to be faster please give it a shot
-	key="\"metadata\":{\"channelMetadataRenderer\":{\"title\":"	
+	if(not video):
+		key="\"metadata\":{\"channelMetadataRenderer\":{\"title\":"	
+	else:
+		key ="\"author\":"	
 	with urllib.request.urlopen(link) as response:
    		messOstring = response.read().decode('utf-8')
 	target_ind = messOstring.index(key)+len(key)
@@ -45,6 +48,7 @@ def run():
 				ytRSS.printRed("Please enter y/n.")
 			elif create_new in yes:
 				create = True
+				loop=False
 			else:
 				loop=False
 	loop=True
@@ -65,10 +69,10 @@ def run():
 				ytRSS.printRed("exiting")
 				exit()
 	loop=True
-
+	video=False
 	while loop:
 		 try:
-		 	channel= input("Enter channel link. [Enter to stop adding.]\n")
+		 	channel= input("Enter youtube link. [Enter to stop adding.]\n")
 		 except Exception as e:
 		 	ytRSS.printRed("Unexpected Error!")
 		 	print(e)
@@ -78,15 +82,16 @@ def run():
 		 	if(channel==""): loop=False
 		 	channel_split=channel.split("/")
 		 	if channel_split[len(channel_split)-2] == "channel":
-		 		print("channel")
+		 		print("Detected as channel link")
 		 		channelName = crawlForUserName(channel)
-		 		print(channelName)
 		 	elif channel_split[len(channel_split)-2] == "c" or channel_split[len(channel_split)-2] == "user" :
-		 		print("user")
+		 		print("Detected as user link")
 		 		channelName = crawlForUserName(channel)
-		 		print(channelName)
 		 	elif "watch" in channel_split[len(channel_split)-1]:
-		 		print("video")
+		 		print("Detected as video link")
+		 		video=True
+		 		channelName = crawlForUserName(channel,video)
+		 	print("Added "+ channelName+" to "+csvfilename)
 
 if __name__== "__main__":
 	run()
